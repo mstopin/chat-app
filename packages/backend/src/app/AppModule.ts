aa3985cea96a3f26d90a9847fae1645ca99fb5c1
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-06 13:55:35 +0200

    feat: add config module and typeorm module

--- packages/backend/src/app/AppModule.ts
+++ packages/backend/src/app/AppModule.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return <TypeOrmModuleOptions>{
+          type: 'postgres' as const,
+          host: configService.getOrThrow<string>('DB_HOST'),
+          port: configService.getOrThrow<number>('DB_PORT'),
+          username: configService.getOrThrow<string>('DB_USER'),
+          password: configService.getOrThrow<string>('DB_PASS'),
+          database: configService.getOrThrow<string>('DB_NAME'),
+          entities: [],
+        };
+      },
+    }),
+  ],
+})
 export class AppModule {}
