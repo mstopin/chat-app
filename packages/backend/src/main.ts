92f6c23c39aeaca25830d6c0833912eb3ba0b2ff
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-06 13:57:47 +0200

    feat: read port from config instead of hardcoded value

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app';
 
 (async function boostrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const APP_PORT = configService.getOrThrow<number>('APP_PORT');
+  await app.listen(APP_PORT);
 })().catch(console.error);
