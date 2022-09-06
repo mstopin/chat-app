50e4490465332e6fa4597bcf36a01f00e4a655f8
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-06 21:37:55 +0200

    feat: set up validation pipe

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './AppModule';
@@ -6,8 +7,15 @@ import { AppModule } from './AppModule';
 (async function boostrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get<ConfigService>(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      disableErrorMessages: true,
+    })
+  );
 
+  const configService = app.get<ConfigService>(ConfigService);
   const APP_PORT = configService.getOrThrow<number>('APP_PORT');
   await app.listen(APP_PORT);
 })().catch(console.error);
