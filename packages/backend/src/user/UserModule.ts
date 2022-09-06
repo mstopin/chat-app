d6ddd119558cbb240f91574042aa07d56beeaa8f
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-06 17:20:27 +0200

    feat: add user service

--- packages/backend/src/user/UserModule.ts
+++ packages/backend/src/user/UserModule.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entities/User';
+import { UserService } from './services/UserService';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
 })
 export class UserModule {}
---
+++ packages/backend/src/user/services/UserService.ts
@@ -0,0 +1,31 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+
+import { User } from '../entities/User';
+
+import { RegisterUserDTO } from './dtos/RegisterUserDTO';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+
+  async registerUser(registerUserDTO: RegisterUserDTO) {
+    const { email, password } = registerUserDTO;
+    const exactEmailUser = await this.userRepository.findOneBy({ email });
+    if (exactEmailUser) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const registeredUser = await this.userRepository.save({
+      email,
+      password: passwordHash,
+    });
+
+    return registeredUser;
+  }
+}
