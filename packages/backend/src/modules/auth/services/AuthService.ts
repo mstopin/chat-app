be977d4ac6356bfad4a4c3f4d303ef180e775213
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-07 16:44:06 +0200

    feat: auth service uses hashing service

--- packages/backend/src/modules/auth/services/AuthService.ts
+++ packages/backend/src/modules/auth/services/AuthService.ts
@@ -1,18 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import bcrypt from 'bcryptjs';
+
+import { HashingService } from '../../../common/modules/HashingModule';
 
 import { UserService } from '../../user/services/UserService';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private hashingService: HashingService
+  ) {}
 
   async validateCredentialsAndGetUser(email: string, password: string) {
     const exactEmailUser = await this.userService.findByEmail(email);
     if (!exactEmailUser) {
       throw new UnauthorizedException();
     }
-    if (!(await bcrypt.compare(password, exactEmailUser.password))) {
+    if (
+      !(await this.hashingService.compare(password, exactEmailUser.password))
+    ) {
       throw new UnauthorizedException();
     }
     return exactEmailUser;
