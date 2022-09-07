973a6e14e110bac95e97e4577bd3cbe3c523687a
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-07 11:04:36 +0200

    feat: add session support with redis as store

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+import session from 'express-session';
+import sessionConnectRedis from 'connect-redis';
 
 import { AppModule } from './AppModule';
 
 (async function boostrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const REDIS_HOST = configService.getOrThrow<string>('REDIS_HOST');
+  const REDIS_PORT = configService.getOrThrow<number>('REDIS_PORT');
+  const redis = new Redis(REDIS_PORT, REDIS_HOST);
+
+  const SESSION_SECRET = configService.getOrThrow<string>('SESSION_SECRET');
+  const RedisSessionStore = sessionConnectRedis(session);
+  app.use(
+    session({
+      cookie: {
+        maxAge: 3 * 24 * 3600,
+        sameSite: 'strict',
+      },
+      secret: SESSION_SECRET,
+      name: 'chat-app.sid',
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisSessionStore({ client: redis }),
+    })
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,7 +39,6 @@ import { AppModule } from './AppModule';
     })
   );
 
-  const configService = app.get<ConfigService>(ConfigService);
   const APP_PORT = configService.getOrThrow<number>('APP_PORT');
   await app.listen(APP_PORT);
 })().catch(console.error);
