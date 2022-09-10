9108ac5b0384ae5e596ba0dfd1e0f553785aceb8
Mateusz Stopiński <46541569+mstopin@users.noreply.github.com>
2022-09-10 16:45:47 +0200

    chore: set up message module

--- packages/backend/src/modules/message/MessageModule.ts
+++ packages/backend/src/modules/message/MessageModule.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { Message } from './entities/Message';
+import { MessageService } from './services/MessageService';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Message])],
+  providers: [MessageService],
+  exports: [MessageService],
+})
 export class MessageModule {}
